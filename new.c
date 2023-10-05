c169f88075a4cf7f73c9914a0f9789ee6c6c060b
Noushad Chaudhary <139552786+0xNoushad@users.noreply.github.com>
2023-10-06 00:35:28 +0530

    Update new.c

--- new.c
+++ new.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
-#include<conio.h>
- 
+
+int main() {
+    double distance;  
+    double time;      
+    double windSpeed; 
+
+    
+    printf("Enter the distance (in meters): ");
+    scanf("%lf", &distance);
+
+    printf("Enter the time (in seconds): ");
+    scanf("%lf", &time);
+
+    
+    windSpeed = distance / time;
+
+    
+    printf("The wind speed is %.2lf meters per second.\n", windSpeed);
+
+    return 0;
+}
